48b64b42ed023d080b17536d1b3156b6d1a66de4
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-05 19:11:28 -0300

    create teacher page

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+import React from 'react';
+import { PageHeader } from '../../Components';
+
+export default () => {
+  return (
+    <div className="container" id="page-teacher-form">
+      <PageHeader  title="Que incrivel que você quer dar aulas." />
+    </div>
+  );
+};
