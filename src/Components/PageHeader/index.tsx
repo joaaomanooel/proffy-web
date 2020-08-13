4756146876d81f31c95d413bfd41b39bbf407ec8
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-13 02:11:42 -0300

    update page header component

--- src/Components/PageHeader/index.tsx
+++ src/Components/PageHeader/index.tsx
@@ -6,10 +6,11 @@ import logo from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg'
 
 interface PageHeaderProps {
-  title: String;
+  title: string;
+  description?: string,
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, description }) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
         {children}
       </div>
     </header>
