20a9d8e516aa0ea3a0ca4ad0f4b0739d8c03c5bf
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-13 02:32:59 -0300

    use diamic values on landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import logo from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
@@ -8,9 +8,18 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import gitClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 
 export default () => {
+  const [connections, setConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('/connections')
+      .then(({ data }) => setConnections(data.total || 0))
+      .catch(e => { console.error(e); setConnections(0); });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -23,17 +32,16 @@ export default () => {
 
         <div className="buttons-container">
           <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar" />
-            Estudar
+            <img src={studyIcon} alt="Estudar" /> Estudar
           </Link>
           <Link to="give-classes" className="give-classes">
-            <img src={gitClassesIcon} alt="Dar aulas" />
-            Dar Aulas
+            <img src={gitClassesIcon} alt="Dar aulas" /> Dar Aulas
           </Link>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas. <img src={purpleHeartIcon}  alt="Coração roxo" />
+          Total de {connections} conexões já realizadas.
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
