08bd95bf2c10dc276b85556b243e2a6d89a7fdad
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-13 02:12:45 -0300

    update teacher item component

--- src/Components/TeacherItem/index.tsx
+++ src/Components/TeacherItem/index.tsx
@@ -2,40 +2,40 @@ import './styles.css';
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
-import { toCurrency, getAleatoryAvatar } from '../../helpers';
+import { toCurrency } from '../../helpers';
+import Teacher from '../../interfaces/Teacher';
+import api from '../../services/api';
 
 interface TeacherItemProps {
-  teacher: {
-    avatar?: string,
-    name: string,
-    title: string,
-    decription: string,
-    price?: number,
-    phone?: string,
-  }
+  teacher: Teacher
 }
 
+const defaultAvatar: string = ' https://api.adorable.io/avatars/285/abott@adorable.png'
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = () => {
+    api.post('connections', { user_id: teacher.id })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src={teacher.avatar || getAleatoryAvatar()} alt={teacher.name} />
+        <img src={teacher.avatar || defaultAvatar} alt={teacher.name} />
         <div>
           <strong>{teacher.name}</strong>
           <span>{teacher.title}</span>
         </div>
       </header>
-      <p>{teacher.decription}</p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora: <strong>{teacher.price ? toCurrency(teacher.price) : 'Grátis'}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.phone}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
