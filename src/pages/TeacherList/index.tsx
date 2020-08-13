9315dfe0c4304d07777d157a0edd11c0cb9c3902
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-13 02:32:08 -0300

    update teacher list

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,49 +1,63 @@
 import './styles.css'
-import React from 'react';
-import { PageHeader, TeacherItem } from '../../Components';
-
-const teachers = [
-  {
-    // avatar: 'https://api.adorable.io/avatars/285/abott@adorable.png',
-    name: 'João N.',
-    title: 'Software Engnier',
-    decription: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    price: 95,
-    phone: '+5551999999999',
-  },
-  {
-    name: 'João N.',
-    title: 'Software Engnier',
-    decription: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    // price: 95,
-    phone: '+5551999999999',
-  },
-]
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { PageHeader, TeacherItem, Input, Select } from '../../Components';
+import { subjects, weekDays } from '../../helpers';
+import api from '../../services/api';
+import Teacher from '../../interfaces/Teacher';
 
 export default () => {
+  const [teachers, setTeachers] = useState([] as Array<Teacher>);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const setValue = (fn: Function) =>
+    ({ target }: ChangeEvent<HTMLInputElement | HTMLSelectElement>) =>
+      fn(target.value);
+
+  const searchTeachers = async (e: FormEvent) => {
+    e.preventDefault();
+    const params = { subject, week_day, time };
+    try {
+      const response = await api.get('classes', { params });
+      setTeachers(response.data);
+    } catch (e) {
+      alert('Erro ao buscar proffys disponíveis.')
+    }
+  }
+
   return (
     <div className="container" id="page-teacher-list">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Matéria"
+            value={subject}
+            options={subjects}
+            onChange={setValue(setSubject)} />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Select
+            name="week_day"
+            value={week_day}
+            options={weekDays}
+            label="Dia da semana"
+            onChange={setValue(setWeekDay)} />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input
+            label="Hora"
+            name="time"
+            type="time"
+            value={time}
+            onChange={setValue(setTime)} />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        {teachers.map(teacher => <TeacherItem teacher={teacher} />)}
+        {teachers.map(teacher => <TeacherItem key={teacher.id} teacher={teacher} />)}
       </main>
     </div>
   );
